2ab8b1d7e0f90b378353c552428c8f10b267392b
Norhan Taher <norhan@lacasacode.come>
2024-07-25 18:19:04 +0300

    add default admin credinials in admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
+        Admin::updateOrCreate(
+            ['email' => 'admin@admin.com'], 
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password')
+            ]
+        );
         Admin::factory()->count(100)->create();
     }
 }
