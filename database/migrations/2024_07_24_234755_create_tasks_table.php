d058ccf901aea0342c2676bb3675a9f91f5d8945
Norhan Taher <norhan@lacasacode.come>
2024-07-25 03:36:35 +0300

    setup project && database models and seeders

---
+++ database/migrations/2024_07_24_234755_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('assigned_to_id')->constrained('users');
+            $table->foreignId('assigned_by_id')->constrained('admins');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
